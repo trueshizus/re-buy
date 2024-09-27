7c6b208b54a5363b805c1f44b3dba8dd1b9bc92d
trueshizus <jesus.herrera.90@gmail.com>
2024-09-27 10:04:51 +0200

    Add dashboard functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,38 @@
+import { createClient } from "@/utils/supabase/server";
+
+const supabase = createClient();
+
+const handleAction = async (formData: FormData) => {
+  "use server";
+
+  const channel = supabase.channel("bids");
+
+  const action = formData.get("action");
+
+  if (!action) return;
+
+  channel.send({
+    type: "broadcast",
+    event: action as string,
+    payload: { message: "Hi" },
+  });
+
+  console.log(action);
+};
+
 export default function Page() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <form action={handleAction}>
+        <button type="submit" name="action" value="start">
+          Start
+        </button>
+        <button type="submit" name="action" value="stop">
+          Stop
+        </button>
+      </form>
+    </div>
+  );
 }
