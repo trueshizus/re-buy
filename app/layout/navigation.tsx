c9b92446bf330e9cd4c39aab61f51f39d2673028
trueshizus <jesus.herrera.90@gmail.com>
2024-09-27 10:20:34 +0200

    set up basic layout

---
+++ app/layout/navigation.tsx
@@ -0,0 +1,7 @@
+export default function Navigation() {
+  return (
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 h-10">
+      Navigation
+    </nav>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,20 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
   return (
     <>
       <Hero />
